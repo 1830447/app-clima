f129c82e1a51f991ee15c7b55bb4142843508b2f
Julio Alberto Zuñiga Gallegos <59932444+1830447@users.noreply.github.com>
2022-09-04 21:11:02 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './assets/css/App.css';
+import NavBar from './components/navBar';
+import WeatherPanel from './components/waterPanel';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <WeatherPanel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navBar.js
@@ -0,0 +1,11 @@
+import '../assets/css/navBar.css';
+const NavBar =()=>{
+    return (
+        <nav className="navbar bg-dark text-light mb-5">
+            <div className="container-fluid">
+                <h3 className="container-fluid"> prediccion Meteorologica</h3>
+            </div>
+        </nav>
+    );
+}
+export default NavBar;
